5cfe8b81c8c847f1fc058553e2d9e21ea9d12ca2
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-02-15 19:45:14 +0330

    example contextMenuStrip

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿let contextMenu = document.querySelector('.context-menu')
+
+let xPosition, yPosition
+
+function showContext(event) {
+    event.preventDefault()
+    xPosition = event.clientX
+    yPosition = event.clientY
+    contextMenu.style.left = `${xPosition}`
+    contextMenu.style.top = `${yPosition}`
+    contextMenu.style.display = 'block'
+    console.log(xPosition, yPosition);
+}
+
+
+function hiddenContext() {
+    contextMenu.style.display = 'none'
+}
+
+document.body.addEventListener('contextmenu', showContext)
+document.body.addEventListener('click', hiddenContext)
+document.body.addEventListener('keydown', hiddenContext)
